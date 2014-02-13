dc4464fa6d97c5be60c19839e1717438026c8143
Eric Brelsford <ebrelsford@gmail.com>
2014-02-13 11:33:43 -0500

    Configurable data dir

--- ladata/load.py
+++ ladata/load.py
@@ -1,8 +1,21 @@
 import os
 
+from django.conf import settings
 
-PROCESSED_DIR = os.path.join(os.path.dirname(__file__), 'processed')
+
+def get_processed_data_dir():
+    """
+    Get the processed data directory, which can be configured by setting
+    LADATA_PROCESSED_DATA_DIR in your settings. Defaults to './processed/'.
+    """
+    try:
+        return settings.LADATA_PROCESSED_DATA_DIR
+    except AttributeError:
+        return os.path.join(os.path.dirname(__file__), 'processed')
 
 
 def get_processed_data_file(name):
-    return os.path.join(PROCESSED_DIR, name)
+    """
+    Get the processed data file with the given name.
+    """
+    return os.path.join(get_processed_data_dir(), name)
