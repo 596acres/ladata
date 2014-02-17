80eef97303a9d65c8481cde123e0d3ce9951f485
Eric Brelsford <ebrelsford@gmail.com>
2014-02-17 11:07:13 -0500

    Fix councildistricts filename

--- ladata/councildistricts/load.py
+++ ladata/councildistricts/load.py
@@ -12,7 +12,7 @@ def from_shapefile(strict=False, progress=True, verbose=False,
     Load council district data into the database from the processed shapefile.
     """
     shp = get_processed_data_file(os.path.join('councildistricts',
-                                               'LA_City_Council_Districts_2012.shp'))
+                                               'councildistricts.shp'))
     mapping = LayerMapping(CouncilDistrict, shp, councildistrict_mapping,
                            transform=True, transaction_mode=transaction_mode)
     mapping.save(strict=strict, progress=progress, verbose=verbose, **kwargs)
