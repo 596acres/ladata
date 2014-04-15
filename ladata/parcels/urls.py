d1077b02c121d1e4ed8a9d339a5ba4c509b5b374
Eric Brelsford <ebrelsford@gmail.com>
2014-04-14 23:16:35 -0400

    Parcel GeoJSON view

---
+++ ladata/parcels/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+
+from .views import GeoJSONPolygonView
+
+
+urlpatterns = patterns('',
+
+    url(r'^geojson/', GeoJSONPolygonView.as_view(), name='parcel_geojson'),
+
+)
---
+++ ladata/parcels/views.py
@@ -0,0 +1,33 @@
+import json
+import geojson
+
+from django.contrib.gis.geos import Polygon
+
+from inplace.views import GeoJSONListView
+from .models import Parcel
+
+
+class GeoJSONPolygonView(GeoJSONListView):
+    model = Parcel
+
+    def get_feature(self, parcel):
+        return geojson.Feature(
+            parcel.pk,
+            geometry=json.loads(parcel.geojson),
+            properties=self.get_properties(parcel),
+        )
+
+    def get_properties(self, parcel):
+        return {
+            'address': parcel.street_address,
+        }
+
+    def get_queryset(self):
+        try:
+            bbox = Polygon.from_bbox(self.request.GET['bbox'].split(','))
+            return super(GeoJSONPolygonView, self).get_queryset().filter(
+                geom__intersects=bbox,
+            ).geojson(precision=6)
+        except KeyError:
+            return Parcel.objects.none()
+
